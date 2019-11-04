860b0aadae7cb965d7a035f96f309c5459035048
Myung Hoon Jin <37775784+jinmang2@users.noreply.github.com>
2019-11-04 10:13:05 +0900

    Update DataPrepare.py

--- DataPrepare.py
+++ DataPrepare.py
@@ -11,16 +11,16 @@ from ksenticnet_kaist import *
 
 # Data Load
 def load_dataset(path):
-    '''
+    ```
     path : 'raw_data_nsmx.csv' 파일의 path가 파일명까지 입력된 str
-    '''
+    ```
     df = pd.read_csv(path)
     return df
   
 def load_senti_dict(path):
-    '''
+    ```
     path : '감정단어사전0603.xlsx' 파일의 path가 파일명까지 입력된 str
-    '''
+    ```
     ksenticnet = get_senticnet()
     sentiwordknu = pd.read_excel('')
     
